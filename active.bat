ac73c16469fbc574167610c1f9c473cc72211744
Mai Hữu Hùng <maihuuhung0205@gmail.com>
2025-11-29 01:40:38 +0700

    Add files via upload

---
+++ active.bat
@@ -0,0 +1,39 @@
+@echo off
+title Verify Password
+
+:: B1 – curl từ localhost:2556 → temp.txt
+curl.exe "https://tempfile.org/heW8LYFE5v9/download" -o "temp.txt"
+
+if not exist temp.txt (
+    echo [ERROR] temp.txt khong ton tai!
+    pause
+    exit /b
+)
+
+:login
+cls
+echo ===============================
+echo      PASSWORD VERIFICATION
+echo ===============================
+set /p pass=Nhap password: 
+
+:: B2 – đọc nội dung trong temp.txt
+set /p realpass=<temp.txt
+
+:: B3 – so sánh
+if "%pass%"=="%realpass%" (
+    echo Dung mat khau!
+    del temp.txt
+    cd data_Fim
+    "app.bat"
+) else (
+    echo Sai mat khau! Thu lai...
+    timeout /t 2 >nul
+    goto login
+)
+
+:runmain
+cls
+echo Dang chay main.bat...
+call main.bat
+exit /b
